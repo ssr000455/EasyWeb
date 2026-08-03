8335e9b8760f977bd007a4660b6f655d0af58944
ssr000455 <3992763243@qq.com>
2026-08-03 08:52:46 +0800

    修复 JVM 目标版本不匹配

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,6 +38,11 @@ android {
         }
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
     kotlinOptions {
         jvmTarget = "17"
     }
